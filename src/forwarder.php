2382a26fb6f97231bad1185c5032a2c2567723d2
Filippo Finke <filippo.finke@samtrevano.ch>
2019-10-24 21:43:11 +0200

    Add TCP forwarder server

---
+++ src/forwarder.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Class that allows you to forward tcp packets from an host to another.
+ * 
+ * Filippo Finke
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Amp\Loop;
+use Amp\Socket\ResourceSocket;
+use Amp\Socket\Server;
+use function Amp\asyncCoroutine;
+use function Amp\Socket\connect;
+
+if(count($argv) != 3) {
+    echo "ℹ️ Usage: php ".$argv[0].".php HOST:PORT FORWARD_TO:PORT" . PHP_EOL;
+    exit;
+}
+$host = $argv[1];
+$forward_to = $argv[2];
+
+Loop::run(static function () use ($host, $forward_to) {
+
+    $forwarder = asyncCoroutine(static function ($from, $to) {
+        [$from_ip, $from_port] = \explode(':', $from->getRemoteAddress());
+        [$to_ip, $to_port] = \explode(':', $to->getRemoteAddress());
+        while (($read = yield $from->read()) !== null) {
+            printf("📡 %s:%d -> %s:%d [%d bytes]".PHP_EOL, $from_ip, $from_port, $to_ip, $to_port, strlen($read));
+            $to->write($read);
+        }
+        $from->close();
+    });
+
+    $server = Server::listen($host);
+
+    echo '⌛ Listening for new connections on ' . $server->getAddress() . ' ...' . PHP_EOL;
+
+    while ($socket = yield $server->accept()) {
+        $socks = yield connect($forward_to);
+        echo '💻 Now forwarding from ' . $server->getAddress() . ' to ' . $socks->getRemoteAddress() . ' for ' . $socket->getRemoteAddress() . PHP_EOL;
+        $forwarder($socket, $socks);
+        $forwarder($socks, $socket);
+    }
+});
