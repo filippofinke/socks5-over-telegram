0d1ea15e2e3da24159cc7077ef739c1ea5025010
Filippo Finke <filippo.finke@samtrevano.ch>
2019-10-25 13:24:33 +0200

    Fixed async call

--- src/forwarder.php
+++ src/forwarder.php
@@ -10,7 +10,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Amp\Loop;
 use Amp\Socket\ResourceSocket;
 use Amp\Socket\Server;
-use function Amp\asyncCoroutine;
+use function Amp\asyncCall;
 use function Amp\Socket\connect;
 
 if(count($argv) != 3) {
@@ -22,15 +22,15 @@ $forward_to = $argv[2];
 
 Loop::run(function () use ($host, $forward_to) {
 
-    $forwarder = asyncCoroutine(function ($from, $to) {
+    $forwarder = function ($from, $to) {
         [$from_ip, $from_port] = \explode(':', $from->getRemoteAddress());
         [$to_ip, $to_port] = \explode(':', $to->getRemoteAddress());
         while (($read = yield $from->read()) !== null) {
             printf("📡 %s:%d -> %s:%d [%d bytes]".PHP_EOL, $from_ip, $from_port, $to_ip, $to_port, strlen($read));
-            $to->write($read);
+            yield $to->write($read);
         }
         $from->close();
-    });
+    };
 
     $server = Server::listen($host);
 
@@ -39,7 +39,7 @@ Loop::run(function () use ($host, $forward_to) {
     while ($socket = yield $server->accept()) {
         $socks = yield connect($forward_to);
         echo '💻 Now forwarding from ' . $server->getAddress() . ' to ' . $socks->getRemoteAddress() . ' for ' . $socket->getRemoteAddress() . PHP_EOL;
-        $forwarder($socket, $socks);
-        $forwarder($socks, $socket);
+        asyncCall($forwarder, $socket, $socks);
+        asyncCall($forwarder, $socks, $socket);
     }
 });
